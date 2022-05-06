aa512aa01cfafa637faa4a47fdf22fe8e0ba78a7
Himel <himelsaha29@live.com>
2022-05-06 12:15:05 -0400

    Error detection for parsed data

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -42,64 +42,100 @@ async function renderChargers() {
     console.log("startttttttttttttttttttttttttttttttttttttttttt");
     charger.forEach(charger => {
 
-        try {
-            var allObj = JSON.parse(JSON.stringify(charger));
+        
+        var allObj = JSON.parse(JSON.stringify(charger));
 
-            var usageType = JSON.parse(JSON.stringify(allObj.UsageType));
+        var usageType = JSON.parse(JSON.stringify(allObj.UsageType));
+        try{
             var isPayAtLocation = usageType.IsPayAtLocation;
             var isMembershipRequired = usageType.IsMembershipRequired;
             var isAccessKeyRequired = usageType.IsAccessKeyRequired;
+        } catch (error) {
+            isPayAtLocation = "N/A";
+            isMembershipRequired = "N/A";
+            isAccessKeyRequired = "N/A";
+        }
 
-            var statusType = JSON.parse(JSON.stringify(allObj.StatusType));
+        var statusType = JSON.parse(JSON.stringify(allObj.StatusType));
+        try{
             var isOperational = statusType.IsOperational;
+        } catch (error) {
+            isOperational = "N/A";
+        }
 
-            var dateLastVerified = JSON.parse(JSON.stringify(allObj.DateLastVerified));
+        var dateLastVerified = JSON.parse(JSON.stringify(allObj.DateLastVerified));
+        if(dateLastVerified == null) {
+            dateLastVerified = "N/A";
+        }
 
-
-
-            var addressInfo= JSON.parse(JSON.stringify(allObj.AddressInfo));
+        var addressInfo= JSON.parse(JSON.stringify(allObj.AddressInfo));
+        try{
             var addressLine1 = addressInfo.AddressLine1;
             var addressLine2 = addressInfo.AddressLine2;
             var town = addressInfo.Town;
             var stateOrProvince = addressInfo.StateOrProvince;
+            var email = addressInfo.ContactEmail;
+        } catch (error) {
+            addressLine1 = "N/A";
+            addressLine2 = "N/A";
+            town = "N/A";
+            stateOrProvince = "N/A";
+            email = "N/A";
+        }
 
-            var country = JSON.parse(JSON.stringify(addressInfo.Country));
+        var country = JSON.parse(JSON.stringify(addressInfo.Country));
+        try{
             var countryTitle = country.Title;
-            
+        } catch (error) {
+            countryTitle = "N/A";
+        }
 
+        try{
             var latitude = addressInfo.Latitude;
             var longitude = addressInfo.Longitude;
-            var email = addressInfo.ContactEmail;
+        } catch (error) {
+            console.log(error);
+
+            // issue
+        }
 
 
-
-            var connections = JSON.parse(JSON.stringify(allObj.Connections))[0];
-            var connectionType = JSON.parse(JSON.stringify(connections.ConnectionType));
+        var connections = JSON.parse(JSON.stringify(allObj.Connections))[0];
+        var connectionType = JSON.parse(JSON.stringify(connections.ConnectionType));
+        try{
             var formalName = connectionType.FormalName;
             var actualConnectionName = connectionType.Title;
-
-            var level = JSON.parse(JSON.stringify(connections.Level));
-            var isFastChargeCapable = level.IsFastChargeCapable;
-            var actualLevel = level.Title;
-
-
             var amps = connections.Amps;
             var voltage = connections.Voltage;
             var powerKW = connections.PowerKW;
-
-
-            var current = JSON.parse(JSON.stringify(connections.CurrentType));
-            var currentType = current.Description;
-
-            var points = allObj.NumberOfPoints;
-
-
-            
-
-
-            
         } catch (error) {
-            console.log(error);
+            formalName = "N/A";
+            actualConnectionName = "N/A";
+            amps = "N/A";
+            voltage = "N/A";
+            powerKW = "N/A";
+        }
+
+        var level = JSON.parse(JSON.stringify(connections.Level));
+        try{
+            var isFastChargeCapable = level.IsFastChargeCapable;
+            var actualLevel = level.Title;
+        } catch (error) {
+            isFastChargeCapable = "N/A";
+            actualLevel = "N/A";
+        }
+
+        var current = JSON.parse(JSON.stringify(connections.CurrentType));
+        try{
+            var currentType = current.Description;
+        } catch (error) {
+            currentType = "N/A";
+        }
+
+        try{
+            var points = allObj.NumberOfPoints;
+        } catch (error) {
+            points = "N/A";
         }
     });
 
